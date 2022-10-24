e873a63245784c6c7eef4e6be7b8d31001968564
YerkoMedinaPichun <yrk.27.medina@gmail.com>
2022-10-24 16:17:06 -0300

    Usando frameworks css para React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Bootstrap from "./components/Bootstrap.js";
+import MaterialUI from "./components/MaterialUI.js";
+import ReactBootstrap from "./components/ReactBootstrap.js";
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
-      <Bootstrap />
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
